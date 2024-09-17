1326dd99556911e9dfa3a0ed906234d81b55dece
Muhammad Saqib <sm4179515@gmail.com>
2024-09-17 11:48:40 +0500

    Add GET request handling for root route '/'

--- server.mjs
+++ server.mjs
@@ -3,8 +3,16 @@ import { createServer } from 'node:http';
 const port = 3000;
 
 const server = createServer((req, res) => {
-  res.end('Server created');
+    const { method, url } = req;
+    
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to the homepage!');
+      }
+      
 });
+
+
 
 server.listen(port, '127.0.0.1', () => {
   console.log(`Server listening on http://127.0.0.1:${port}`);
